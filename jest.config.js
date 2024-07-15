4272631356c1f13f7e1539cb3ad69d76ce77cf3f
minhasmaqsood <minhasmaqsood@gmal.com>
2024-07-15 12:43:55 +0500

    run_test_cases

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
-// jest.config.js
-module.exports = {
-  preset: 'react-native',
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-native-check-box|react-native-button|react-native-snap-carousel|react-native-progress|react-redux)/)',
-  ],
-  setupFilesAfterEnv: [
-    '@testing-library/jest-native/extend-expect',
-  ],
-};
+  module.exports = {
+    preset: 'react-native',
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+    transformIgnorePatterns: [
+      'node_modules/(?!(react-native|@react-native|react-native-check-box|react-native-button|react-native-snap-carousel|react-native-progress|react-redux)/)',
+    ],
+    setupFilesAfterEnv: [
+      '@testing-library/jest-native/extend-expect',
+    ],
+  };
+  
+  
